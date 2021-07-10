b76a7d9997da95d6084f34ab7e05aaaba31100fe
Arjun O K <jasprit1511@gmail.com>
2021-07-10 22:21:25 +0530

    landing page with footer

--- src/components/Content.js
+++ src/components/Content.js
@@ -4,39 +4,51 @@ import React from 'react';
 function Content() {
     return (
         <div className="container">
-            <div className="card-group mx-auto">
-            <div className="card">
-                <img className="card-img-top" src="images/smudge.jpg" alt="card-img"></img>
-                <div className="card-body">
-                    <h6 className="card-title">Smudge Paintings</h6>
-                    <p className="card-text">₹300 (per person)</p>
-                    <button className="btn btn-primary">Order Now</button>
+            <div className="card-group">
+                <div className="row">
+                <div className="card">
+                    <img className="card-img-top" src="images/smudge.jpg" alt="card-img"></img>
+                    <div className="card-body">
+                        <h6 className="card-title">Smudge Paintings</h6>
+                        <b><i className="text-danger float-left">₹300</i></b>
+                        <i className="text-muted ml-2 off-price">₹400</i>
+                        <button className="btn btn-primary float-right">Order Now</button>
+                    </div>
                 </div>
-            </div>
-            <div className="card">
-                <img className="card-img-top" src="images/caricature.jpg" alt="card-img"></img>
-                <div className="card-body">
-                    <h6 className="card-title">Caricatures</h6>
-                    <p className="card-text">₹500 (per person)</p>
-                    <button className="btn btn-primary">Order Now</button>
                 </div>
-            </div>
-            <div className="card">
-                <img className="card-img-top" src="images/manipulation.jpg" alt="card-img"></img>
-                <div className="card-body">
-                    <h6 className="card-title">Manipulations</h6>
-                    <p className="card-text">₹300 (per person)</p>
-                    <button className="btn btn-primary">Order Now</button>
+                <div className="row">
+                <div className="card">
+                    <img className="card-img-top" src="images/caricature.jpg" alt="card-img"></img>
+                    <div className="card-body">
+                        <h6 className="card-title">Caricatures</h6>
+                        <b><i className="text-danger float-left">₹500</i></b>
+                        <i className="text-muted ml-2 off-price">₹600</i>
+                        <button className="btn btn-primary float-right">Order Now</button>
+                    </div>
                 </div>
-            </div>
-            <div className="card">
-                <img className="card-img-top" src="images/illustrator.jpg" alt="card-img"></img>
-                <div className="card-body">
-                    <h6 className="card-title">Illustrations</h6>
-                    <p className="card-text">₹500 (per person)</p>
-                    <button className="btn btn-primary">Order Now</button>
                 </div>
-            </div>
+                <div className="row">
+                <div className="card">
+                    <img className="card-img-top" src="images/manipulation.jpg" alt="card-img"></img>
+                    <div className="card-body">
+                        <h6 className="card-title">Manipulations</h6>
+                        <b><i className="text-danger float-left">₹300</i></b>
+                        <i className="text-muted ml-2 off-price">₹400</i>
+                        <button className="btn btn-primary float-right">Order Now</button>
+                    </div>
+                </div>
+                </div>
+                <div className="row">
+                <div className="card">
+                    <img className="card-img-top" src="images/illustrator.jpg" alt="card-img"></img>
+                    <div className="card-body">
+                        <h6 className="card-title">Illustrations</h6>
+                        <b><i className="text-danger float-left">₹500</i></b>
+                        <i className="text-muted ml-2 off-price">₹600</i>
+                        <button className="btn btn-primary float-right">Order Now</button>
+                    </div>
+                </div>
+                </div>
             </div>
         </div>
     );
--- src/components/footer.js
+++ src/components/footer.js
@@ -3,20 +3,19 @@ import './footer.css';
 
 function Footer() {
     return (
-        <footer className="footer bg-light">
-            <div className="py-5 mt-5 mb-0">
-                <ul class="list-inline d-flex justify-content-center">
-                    <li class="list-inline-item "><a href="#" target="_blank" title="twitter"><i class="fa fa-twitter fa-2x"></i></a></li>
-                    <li class="list-inline-item"><a href="#" target="_blank" title="facebook"><i class="fa fa-facebook fa-2x"></i></a></li>
-                    <li class="list-inline-item"><a href="#" target="_blank" title="instagram"><i class="fa fa-instagram fa-2x"></i></a></li>
+        <div class="footer-basic">
+            <footer>
+                <div class="social"><a href="#"><i class="fa fa-instagram"></i></a><a href="#"><i class="fa fa-twitter"></i></a><a href="#"><i class="fa fa-facebook"></i></a><a href="#"><i class="fa fa-envelope"></i></a></div>
+                <ul class="list-inline">
+                    <li class="list-inline-item"><a href="#">Home</a></li>
+                    <li class="list-inline-item"><a href="#">Services</a></li>
+                    <li class="list-inline-item"><a href="#">About</a></li>
+                    <li class="list-inline-item"><a href="#">Terms</a></li>
+                    <li class="list-inline-item"><a href="#">Privacy Policy</a></li>
                 </ul>
-            </div>
-            <div class="bg-light">
-                <div class="text-center">
-                    <p class="text-muted mt-0 py-2">© 2019 Bootstrapious All rights reserved.</p>
-                </div>
-            </div>
-        </footer>
+                <p class="copyright">OK Arjun Studio © 2021</p>
+            </footer>
+        </div>
     );
 }
 
